a839cf14206603f642db37019133d8388a81a9ca
ricarellano <rarellanob@gmail.com>
2016-12-11 19:32:13 -0800

    airplane working

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 $(document).ready( function() {
 
-  var board = [];
+
   var colLength = 10;
   var rowLength = 13;
 
@@ -15,17 +15,23 @@ $(document).ready( function() {
     }
     matrix.push(array);
   }
-  console.log(matrix)
   //move airplane
 
   // matrix[4][0] = true;
   $('#40').append('<img src="airplane.jpg" alt="airplane" id="airplane">');
+  // $('#47').append('<img src="bird.jpg" alt="bird" id="bird">');
+  // var birdImg = ('<img src="bird.jpg" alt="bird" id="bird">');
+
   var row = 4;
-  var col = 0;
+  var col = 1;
 
 $('.start').click(function (){
+  createBirds();
   async.eachSeries(matrix[row], function(element, callback) {
     setTimeout(function() {
+      if(matrix[row][col] === 1){
+        alert("crash");
+      }
       if (col != 0) {
         matrix[row][col-1] = null;
         $("#airplane").remove();
@@ -33,11 +39,16 @@ $('.start').click(function (){
       matrix[row][col] = true;
       var position = "#" + row + col;
       $(position).append('<img src="airplane.jpg" alt="airplane" id="airplane">');
+      if(col === 9){
+        alert("you win!")
+      }
       col++;
       callback();
-    }, 1000);
+    }, 700);
   }, function(err) {
+
     console.log("Terminó");
+
   });
 
 });
@@ -65,22 +76,50 @@ $(window).keydown(function(event){
 });
 
 
+
+
+
+
 var birds = [
-  matrix[4][6]
+    matrix[6][6] = false
 ];
 
 
-
-  function createBird (){
-    if ($('#airplane') === $('#47')){
-
-      console.log("la cagaste");
-
-
+  function createBirds (){
+    var i = 0;
+    var birds = [];
+    while (i < 5){
+      var random = Math.random()*129;
+      var birdRandom = Math.floor(random);
+      if( birdRandom > 43 || birdRandom < 40){
+        addBirdInMatrix(birdRandom);
+        if(birdRandom < 10){
+          birdRandom = "0" + birdRandom;
+        }
+        var position = '#' + birdRandom;
+        $(position).append('<img src="bird.jpg" alt="bird" id="bird">');
+        i++;
+      }
     }
   }
-createBird ();
+  console.log(matrix);
 
+function addBirdInMatrix (birdRandom){
+  var birdRandomString = birdRandom.toString()
+  if(birdRandomString.length === 1){
+    matrix[0][Number(birdRandomString)] = 1;
+  }
+  else if (birdRandomString.length === 3){
+    var rowBird = birdRandomString[0] + birdRandomString[1];
+    var colBird = birdRandomString[2];
+    matrix[rowBird][colBird] = 1;
+  }
+  else {
+    var rowBird = birdRandomString[0];
+    var colBird = birdRandomString[1];
+    matrix[rowBird][colBird] = 1;
+  }
+}
 
 
 
